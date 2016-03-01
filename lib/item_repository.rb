697b8a9c891ff5e56b247e306c3ee71e7d608663
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-03-01 16:18:26 -0700

    build item repository

---
+++ lib/item_repository.rb
@@ -0,0 +1,50 @@
+require_relative 'item'
+require 'bigdecimal'
+
+class ItemRespository
+  
+  def initialize(items = [])
+    @items = items
+  end
+
+  def all
+    @items
+  end
+
+  def find_by_id(num)
+    @items.find do |item|
+      item.id == num
+    end
+  end
+
+  def find_by_name(name)
+    @items.find do |item|
+      item.name.downcase.include?(name.downcase)
+    end
+  end
+
+  def find_all_with_description(description)
+    @items.find_all do |item|
+      item.description.downcase.include?(description.downcase)
+    end
+  end
+
+  def find_all_by_price(price)
+    @items.find_all do |item|
+      item.unit_price == price
+    end
+  end
+
+  def find_all_by_price_in_range(low, high)
+    @items.find_all do |item|
+      item.unit_price > low && item.unit_price < high
+    end
+  end
+
+  def find_all_by_merchant_id(id)
+    @items.find_all do |item|
+      item.merchant_id == id
+    end
+  end
+
+end
---
+++ test/item_repository_test.rb
@@ -0,0 +1,88 @@
+require_relative 'test_helper'
+require_relative '../lib/item'
+require_relative '../lib/item_repository'
+require 'pry'
+
+
+class ItemRespositoryTest < Minitest::Test
+
+  def setup
+
+    @pencil = Item.new({
+        :id          => 145685852,
+        :name        => "Pencil",
+        :description => "You can use it to write things",
+        :unit_price  => BigDecimal.new(21.99,4),
+        :created_at  => Time.new(2016,03,1,11,24,14, "+07:00"),
+        :updated_at  => Time.new(2016,03,1,11,24,14, "+07:00"),
+        :merchant_id => 20
+      })
+
+    @paper = Item.new({
+        :id          => 145685851,
+        :name        => "Paper",
+        :description => "You can use it to write things on",
+        :unit_price  => BigDecimal.new(21.99,4),
+        :created_at  => Time.new(2016,03,1,11,24,14, "+08:00"),
+        :updated_at  => Time.new(2016,03,1,11,24,14, "+08:00"),
+        :merchant_id => 10
+      })
+
+    @eraser = Item.new({
+        :id          => 145685850,
+        :name        => "Eraser",
+        :description => "You can use it to erase",
+        :unit_price  => BigDecimal.new(41.99,4),
+        :created_at  => Time.new(2016,03,1,11,24,14, "+09:00"),
+        :updated_at  => Time.new(2016,03,1,11,24,14, "+09:00"),
+        :merchant_id => 20
+      })
+
+      @repo = ItemRespository.new([@pencil, @paper, @eraser])
+      @repo2 = ItemRespository.new
+
+  end
+
+  def test_it_returns_an_array_of_all_known_item_instances
+    assert_equal [@pencil, @paper, @eraser], @repo.all
+    assert_equal [], @repo2.all
+  end
+
+  def test_it_returns_an_instance_of_item_with_a_matching_id
+    assert_equal @pencil, @repo.find_by_id(145685852)
+    assert_equal nil, @repo.find_by_id(9283749284)
+  end
+
+  def test_it_returns_an_item_having_done_a_case_insensitive_search
+    assert_equal @paper, @repo.find_by_name("Paper")
+    assert_equal @paper, @repo.find_by_name("PAPER")
+    assert_equal @paper, @repo.find_by_name("PAPE")
+    assert_equal nil, @repo.find_by_name("hello")
+  end
+
+  def test_it_returns_items_where_the_supplied_string_appears_in_the_description
+    assert_equal [@pencil, @paper, @eraser],  @repo.find_all_with_description("You can use it")
+    assert_equal [@pencil, @paper, @eraser],  @repo.find_all_with_description("YOU CAN USE IT")
+    assert_equal [@eraser], @repo.find_all_with_description("erase")
+    assert_equal [], @repo.find_all_with_description("hello")
+  end
+
+  def test_it_returns_items_matching_the_given_price
+    assert_equal [@pencil, @paper], @repo.find_all_by_price(BigDecimal.new(21.99,4))
+    assert_equal [], @repo.find_all_by_price(BigDecimal.new(11.99,4))
+  end
+
+  def test_it_returns_items_within_the_given_price_range
+    too_low = BigDecimal.new(10.99,4)
+    low = BigDecimal.new(20.99,4)
+    high = BigDecimal.new(50.99,4)
+    
+    assert_equal [@pencil, @paper, @eraser],  @repo.find_all_by_price_in_range(low, high)
+    assert_equal [], @repo.find_all_by_price_in_range(too_low, low)
+  end
+
+  def test_it_returns_items_matching_the_given_merchant_id
+    assert_equal [@pencil, @eraser], @repo.find_all_by_merchant_id(20)
+    assert_equal [], @repo.find_all_by_merchant_id(30)
+  end
+end
