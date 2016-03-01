c3d2a31c4820c835ecf6aa606556721ef6d402fe
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-03-01 13:06:42 -0700

    add item class and tests

--- test/item_test.rb
+++ test/item_test.rb
@@ -6,50 +6,41 @@ require 'pry'
 class ItemTest < Minitest::Test
   def setup
           @item = Item.new({
-        :name        => "Pencil",
-        :description => "You can use it to write things",
-        :unit_price  => BigDecimal.new(10.99,4),
-        :created_at  => Time.now,
-        :updated_at  => Time.now,
-      })
+              :id          => 145685852,
+              :name        => "Pencil",
+              :description => "You can use it to write things",
+              :unit_price  => BigDecimal.new(10.99,4),
+              :created_at  => Time.new(2016,03,1,11,24,14, "+07:00"),
+              :updated_at  => Time.new(2016,03,1,11,24,14, "+07:00"),
+            })
   end
-
-  #still need to write a takedown method that will erase the item
-  #that was appended to file in each test
 
   def test_it_returns_the_integer_id_of_the_item
-    skip
-    assert_equal sumnum, @item.id
+    assert_equal 145685852, @item.id
   end
 
-  def test_it_returns_the_name_of_the_item #passing
+  def test_it_returns_the_name_of_the_item
     assert_equal "Pencil", @item.name
   end
 
-  def test_it_returns_the_description_of_the_item #passing
+  def test_it_returns_the_description_of_the_item
     assert_equal "You can use it to write things", @item.description
   end
 
-  def test_it_returns_the_price_of_the_item #passing
+  def test_it_returns_the_price_of_the_item
     assert_equal BigDecimal.new(10.99,4), @item.unit_price
   end
 
   def test_it_returns_a_Time_instance_for_the_date_the_item_was_first_created
-    skip
-    #not sure how we'll test this one...
-    assert_equal Time.now, @item.created_at
+    assert_equal Time.new(2016,03,1,11,24,14, "+07:00"), @item.created_at
   end
 
   def test_it_returns_a_Time_instance_for_the_date_the_item_was_last_modified
-    skip
-    #need to modify the file to run this test..create a special file with a single,
-    #modification and run the test on that?
-    assert_equal somedate, @item.updated_at
+    assert_equal Time.new(2016,03,1,11,24,14, "+07:00"), @item.updated_at
   end
 
   def test_it_returns_the_integer_merchant_id_of_the_item
-    skip
-    assert_equal sumnum, @item.merchant_id
+    assert_equal 0, @item.merchant_id
   end
 
 end
